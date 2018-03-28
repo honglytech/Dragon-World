5500902257b53a650fea532e8c1b3367c16012ac
Hong Ly <lyhong.rupp@gmail.com>
2018-03-28 16:46:04 +1100

    Add Game Logo & Test Key Press

---
+++ Assets/TextController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour {
+
+	public Text text;
+
+
+	// Use this for initialization
+	void Start () {
+		text.text = "Hello World";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			text.text = "Space key pressed";
+		}
+	}
+}

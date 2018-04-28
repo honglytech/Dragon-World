3e34cb704c85db587cc9e6dc34bbf213f8c22321
Hong Ly <lyhong.rupp@gmail.com>
2018-04-29 00:06:39 +1000

    Json file added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,20 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TextDragonWorldRPG;
+using System.IO;
 
 public class Player : Character
 {
+    string path;
+    string jsonString;
 
-	public int Floor { get; set; }
-
+    public int Floor { get; set; }
+    
 	// Use this for initialization
 	void Start () {
+
+		path = Application.streamingAssetsPath + "/player.json"; 
+		jsonString = File.ReadAllText(path);
+		playerJson gamePlayer = JsonUtility.FromJson<playerJson>(jsonString);
+		Debug.Log(gamePlayer.Attack);
+
+
 		// For now, these variables are used for testing 
-		Floor = 0; 
-		Energy = 25;
-		Attack = 15;
-		Defence = 6;
-		Gold = 0;
+		//Floor = 0; 
+		//Energy = 25;
+		//Attack = 15;
+		//Defence = 6;
+		//Gold = 0;
 		Inventory = new List<string> ();
 		DungeonIndex = new Vector2 (2, 2);
 	}
@@ -38,4 +48,14 @@ public class Player : Character
 		Debug.Log ("Player, Game over!");
 		base.Die ();
 	}
+}
+
+[System.Serializable]
+public class playerJson
+{
+	public int Floor;
+	public int Energy;
+	public int Attack;
+	public int Defence;
+	public int Gold;
 }

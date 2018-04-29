2d9093f4edfbde33efd209ef26fe6f95a9cc75d0
Hong Ly <lyhong.rupp@gmail.com>
2018-04-29 22:29:49 +1000

    Dragon & EvilSpirit classes support json files

--- Assets/Scripts/Dragon.cs
+++ Assets/Scripts/Dragon.cs
@@ -2,19 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TextDragonWorldRPG;
+using System.IO;
 
 // Dragon class is inherited from Enemy class 
 public class Dragon : Enemy
 {
-	
+	string path;
+	string jsonString;
+
 	// Use this for initialization
 	void Start ()
 	{	
+		path = Application.streamingAssetsPath + "/dragon.json"; 
+		jsonString = File.ReadAllText(path);
+		dragonJson gameDragon = JsonUtility.FromJson<dragonJson>(jsonString);
+		//Debug.Log(gameDragon.Attack);
+
 		// For now, these variables are used for testing 
-		Energy = 20;
-		Attack = 8;
-		Defence = 5;
-		Gold = 15;
+		//Energy = 20;
+		Attack = gameDragon.Attack;
+		//Debug.Log(Attack);
+		//Defence = 5;
+		//Gold = 15;
 		Inventory.Add ("Wing");
 	}
+}
+
+[System.Serializable]
+public class dragonJson
+{
+	public int Energy;
+	public int Attack;
+	public int Defence;
+	public int Gold;
 }
--- Assets/Scripts/EvilSpirit.cs
+++ Assets/Scripts/EvilSpirit.cs
@@ -2,18 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TextDragonWorldRPG;
+using System.IO;
 
 // Dragon class is inherited from Enemy class 
 public class EvilSpirit : Enemy
 {
-	
+	string path;
+	string jsonString;
+
 	// Use this for initialization
 	void Start () {
+
+		path = Application.streamingAssetsPath + "/evilSpirit.json"; 
+		jsonString = File.ReadAllText(path);
+		evilSpiritJson evilSpiritDragon = JsonUtility.FromJson<evilSpiritJson>(jsonString);
+
 		// For now, these variables are used for testing 
-		Energy = 10;
-		Attack = 4;
-		Defence = 3;
-		Gold = 20;
+		Gold = evilSpiritDragon.Gold;
+		Debug.Log(Gold);
+		//Attack = 4;
+		//Defence = 3;
+		//Gold = 20;
 		Inventory.Add ("Eyes");
 	}
+}
+
+[System.Serializable]
+public class evilSpiritJson
+{
+	public int Energy;
+	public int Attack;
+	public int Defence;
+	public int Gold;
 }

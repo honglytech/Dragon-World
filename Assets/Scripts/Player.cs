1f39d06cc9aa89b6d781422fffc656d6e5ae1d3f
Hong Ly <lyhong.rupp@gmail.com>
2018-05-01 22:25:49 +1000

    Move direction method

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,7 +10,12 @@ public class Player : Character
     string jsonString;
 
     public int Floor { get; set; }
-    
+	// Track current player location
+	public Dungeon CurrentLocation { get; set; }
+	// Can be accessible in the inspector 
+	[SerializeField]
+	DragonWorld world; 
+
 	// Use this for initialization
 	void Start () {
 
@@ -28,7 +33,62 @@ public class Player : Character
 		//Gold = 0;
 		Inventory = new List<string> ();
 		DungeonIndex = new Vector2 (2, 2);
+		// Set up the dungeon for navigation 
+		world.Dungeon[(int)DungeonIndex.x, (int)DungeonIndex.y].Empty = true;
 		AddItem("A lot of items");
+	}
+
+	public void Move(int direction)
+	{
+		// If the current player location has an enemy, then stop execution
+		if (this.CurrentLocation.Enemy) 
+		{
+			return;
+		}
+
+		/* 
+		 * Map view as dungeon index 10 x 10
+		 +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+		 | 0,0 | 1,0 | 2,0 | 3,0 | 4,0 | 5,0 | 6,0 | 7,0 | 8,0 | 9,0 |
+		 +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+		 | 0,1 | 1,1 | 2,1 | 3,1 | 4,1 | 5,1 | 6,1 | 7,1 | 8,1 | 9,1 |
+		 +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+		 | 0,2 | 1,2 | 2,2 | 3,2 | 4,2 | 5,2 | 6,2 | 7,2 | 8,2 | 9,2 |
+		 +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+		 | 0,3 | 1,3 | 2,3 | 3,3 | 4,3 | 5,3 | 6,3 | 7,3 | 8,3 | 9,3 |
+		 +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+		 | 0,4 | 1,4 | 2,4 | 3,4 | 4,4 | 5,4 | 6,4 | 7,4 | 8,4 | 9,4 |
+		 +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+		 | 0,5 | 1,5 | 2,5 | 3,5 | 4,5 | 5,5 | 6,5 | 7,5 | 8,5 | 9,5 |
+		 +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+		 | 0,6 | 1,6 | 2,6 | 3,6 | 4,6 | 5,6 | 6,6 | 7,6 | 8,6 | 9,6 |
+		 +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+		 | 0,7 | 1,7 | 2,7 | 3,7 | 4,7 | 5,7 | 6,7 | 7,7 | 8,7 | 9,7 |
+		 +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+		 | 0,8 | 1,8 | 2,8 | 3,8 | 4,8 | 5,8 | 6,8 | 7,8 | 8,8 | 9,8 |
+		 +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+		 | 0,9 | 1,9 | 2,9 | 3,9 | 4,9 | 5,9 | 6,9 | 7,9 | 8,9 | 9,9 |
+		 +-----+-----+-----+-----+-----+-----+-----+-----+-----+-----+
+		 */
+
+		switch (direction)
+		{
+			// Move north
+			case 0:
+			
+				break;
+			// Move east
+			case 1: 
+				break;
+			// Move south
+			case 2:
+
+				break;
+			// Move west
+			case 3: 
+				break;
+		}
+
 	}
 
 	public void AddItem(string item)

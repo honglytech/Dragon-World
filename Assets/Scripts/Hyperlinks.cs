23bc8c0c5ce22ee5117603290073c725cca3725d
Hong Ly <lyhong.rupp@gmail.com>
2018-05-14 22:31:22 +1000

    Legal section and Grading Criteria.
    Added legal section under About page with Hyperlinks class and Updated grading criteria.

---
+++ Assets/Scripts/Hyperlinks.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Hyperlinks is a class that controls hyperlink in Legal section under about page.
+public class Hyperlinks : MonoBehaviour {
+
+    // First attribute to the first image in about page.
+    public void OpenHyperlinkOne()
+    {
+        Application.OpenURL("https://www.pinterest.co.uk/pin/391039180118906812/?autologin=true");
+    }
+
+    // Second attribute to the second image in about page.
+    public void OpenHyperlinkTwo()
+    {
+        Application.OpenURL("https://dashinvaine.deviantart.com/art/Dragon-and-Captive-reloaded-129209195");
+    }
+}
